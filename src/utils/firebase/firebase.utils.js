5fe955e4168f96f0d375ba8f0a5902d7da92aa4d
Unknown-Coder769 <unknowncoder769@gmail.com>
2023-02-27 00:28:05 +0500

    Firebase changes

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -8,12 +8,12 @@ import {
 import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDDU4V-_QV3M8GyhC9SVieRTDM4dbiT0Yk",
-  authDomain: "crwn-clothing-db-98d4d.firebaseapp.com",
-  projectId: "crwn-clothing-db-98d4d",
-  storageBucket: "crwn-clothing-db-98d4d.appspot.com",
-  messagingSenderId: "626766232035",
-  appId: "1:626766232035:web:506621582dab103a4d08d6",
+  apiKey: "AIzaSyAEwaTaX6ppY5zxSrYhIH_38O-o-6pDq-A",
+  authDomain: "crwn-clothing-v2-63885.firebaseapp.com",
+  projectId: "crwn-clothing-v2-63885",
+  storageBucket: "crwn-clothing-v2-63885.appspot.com",
+  messagingSenderId: "248915096527",
+  appId: "1:248915096527:web:3708954b8d1ef2857718ba",
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
