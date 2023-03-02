8b79e7718de417358f0b3c81c154c49b7114d999
Unknown-Coder769 <unknowncoder769@gmail.com>
2023-03-03 00:37:21 +0500

    Context Providers added to prevent prop drilling

--- src/index.js
+++ src/index.js
@@ -1,18 +1,25 @@
-import React from "react";
-import { render } from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+import React from 'react';
+import { render } from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 
-import { UserProvider } from "./contexts/user.context";
-import App from "./App";
-import "./index.scss";
+import App from './App';
+import { UserProvider } from './contexts/user.context';
+import { ProductsProvider } from './contexts/products.context';
+import { CartProvider } from './contexts/cart.context';
 
-const rootElement = document.getElementById("root");
+import './index.scss';
+
+const rootElement = document.getElementById('root');
 
 render(
   <React.StrictMode>
     <BrowserRouter>
       <UserProvider>
-        <App />
+        <ProductsProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </ProductsProvider>
       </UserProvider>
     </BrowserRouter>
   </React.StrictMode>,
