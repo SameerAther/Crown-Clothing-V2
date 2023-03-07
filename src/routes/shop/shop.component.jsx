17c84bb15694764cbd2b87ccbf8641d0914ff7a5
Unknown-Coder769 <unknowncoder769@gmail.com>
2023-03-08 01:25:11 +0500

    useSelector trigger added and momoization added through reselect library

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -5,16 +5,16 @@ import { useDispatch } from "react-redux";
 import CategoriesPreview from "../categories-preview/categories-preview.component";
 import Category from "../category/category.component";
 import { getCategoriesAndDocuments } from "../../utils/firebase/firebase.utils";
-import { setCategoriesMap } from "../../store/categories/categories.action";
+import { setCategories } from "../../store/categories/categories.action";
 
 const Shop = () => {
   const dispatch = useDispatch();
   useEffect(() => {
-    const getCategoriesMap = async () => {
-      const categoryMap = await getCategoriesAndDocuments();
-      dispatch(setCategoriesMap(categoryMap));
+    const getCategories = async () => {
+      const categoryArray = await getCategoriesAndDocuments();
+      dispatch(setCategories(categoryArray));
     };
-    getCategoriesMap();
+    getCategories();
   }, [dispatch]);
   return (
     <Routes>
